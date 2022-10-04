9e18bd7180b9915adcbaff1adbe67e31dfd81011
zych.johann@gmail.com <zych.johann@gmail.com>
2022-10-04 10:16:40 +0200

    Correction

--- thanks.php
+++ thanks.php
@@ -50,9 +50,9 @@ if (count($errors) > 0) : ?>
 <main class="container">
     <div class="text-center border rounded p-3 m-5">
         <h2>Vous êtes inscrit !</h2>
-        <p>Merci <?= $_POST['firstName'] . " " .  $_POST['lastName'] ?> de nous avoir contacté à propos de " <?= $_POST['subject'] ?>".
+        <p>Merci <?= $_POST["firstName"] . " " .  $_POST["lastName"] ?> de nous avoir contacté à propos de " <?= $_POST["subject"] ?>".
 
-            Un de nos conseiller vous contactera soit à l’adresse <?= $_POST['email'] ?> ou par téléphone au <?= $_POST['phone'] ?> dans les plus brefs délais pour traiter votre demande :
+            Un de nos conseiller vous contactera soit à l’adresse <?= $_POST["email"] ?> ou par téléphone au <?= $_POST["phone"] ?> dans les plus brefs délais pour traiter votre demande :
 
             <?= $_POST['message'] ?>.</p>
     </div>
